819779e553ddf01eb72cb441b97379c66b19e2cd
Jimmy Shah <jimmyshah83@gmail.com>
2021-09-29 09:06:26 -0400

    Add Sleep to showcase autoscaling

--- autoscale.sh
+++ autoscale.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..20}
+for i in {1..50}
 do
   curl http://knative-demo.default.127.0.0.1.nip.io/demo 
   printf "\n"
--- src/main/java/com/knative/demo/web/KNativeDemoController.java
+++ src/main/java/com/knative/demo/web/KNativeDemoController.java
@@ -1,10 +1,13 @@
 package com.knative.demo.web;
 
 import lombok.Getter;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/demo")
@@ -14,8 +17,10 @@ public class KNativeDemoController {
     @Value("${message.value}")
     private String message;
 
+    @SneakyThrows
     @GetMapping
     public String showMessage() {
+        TimeUnit.SECONDS.sleep(2);
         return this.getMessage();
     }
 }
